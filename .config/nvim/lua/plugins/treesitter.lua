7c4b65bae056f58efb3d938edb5f627c301a1c74
Artur Shevtsov <ashevtsov@luftronix.com>
2025-08-21 17:19:48 +0300

    add treesitter text objects to treesitter setup

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,10 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
