906d109b8dd813979cd0f8728fc1e7700b398549
Artur Shevtsov <ashevtsov@luftronix.com>
2025-08-22 15:42:28 +0300

    small tweak & cleanup for colorscheme

--- .config/nvim/colors/blueberrypie.lua
+++ .config/nvim/colors/blueberrypie.lua
@@ -1,23 +1,22 @@
--- ~/.config/nvim/colors/blueberrypie.lua
 local colors = {
-  bg       = "#1c0c28", -- same as Ghostty background
-  fg       = "#ffffff", -- main foreground
-  cursor   = "#00ffff",
-  cursor_fg= "#000000",
-  sel_bg   = "#606060",
-  sel_fg   = "#ffffff",
+  bg         = "#1c0c28",
+  fg         = "#ffffff",
+  cursor     = "#00ffff",
+  cursor_fg  = "#000000",
+  sel_bg     = "#606060",
+  sel_fg     = "#ffffff",
 
-  -- Accents
-  cyan     = "#5cb1b3",
-  magenta  = "#9d54a7",
-  purple   = "#7e83cc",
-  pink     = "#bc94b7",
-  red      = "#c87272",
-  blue     = "#90a5bd",
-  green    = "#0aff99",
-  lgreen   = "#2ee6a6",
-  yellow   = "#ffd75f",
-  gray     = "#5e6071",
+  -- Accents 
+  cyan       = "#5cb1b3",
+  magenta    = "#9d54a7",
+  purple     = "#7e83cc",
+  pink       = "#bc94b7",
+  red        = "#c87272",
+  blue       = "#90a5bd",
+  green      = "#0aff99",
+  lgreen     = "#c6e960",
+  yellow     = "#ffd75f",
+  gray       = "#5e6071",
 }
 
 vim.cmd("highlight clear")
@@ -33,14 +32,13 @@ vim.api.nvim_set_hl(0, "Cursor",       { fg = colors.cursor_fg, bg = colors.curs
 vim.api.nvim_set_hl(0, "Visual",       { bg = colors.sel_bg })
 vim.api.nvim_set_hl(0, "Search",       { fg = colors.bg, bg = colors.green, bold = true })
 vim.api.nvim_set_hl(0, "LineNr",       { fg = colors.gray })
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = colors.green, bold = true })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = colors.lgreen, bold = true })
 vim.api.nvim_set_hl(0, "StatusLine",   { fg = colors.fg, bg = colors.gray })
 vim.api.nvim_set_hl(0, "VertSplit",    { fg = colors.gray })
 
 -- Syntax
 vim.api.nvim_set_hl(0, "Comment",      { fg = colors.gray, italic = true })
 vim.api.nvim_set_hl(0, "Constant",     { fg = colors.cyan })
--- vim.api.nvim_set_hl(0, "String",       { fg = colors.green })
 vim.api.nvim_set_hl(0, "String",       { fg = colors.lgreen })
 
 vim.api.nvim_set_hl(0, "Character",    { fg = colors.pink })
