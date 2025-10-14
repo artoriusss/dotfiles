2ef221d495c12a31aca5b0819e3f01053f481146
Artur <ashevtsov@luftronix.com>
2025-10-14 09:30:36 +0300

    add workaround solution for "No Visual Selection Found" on :MoltenEvaluateVisual keybind

--- .config/nvim/lua/plugins/molten.lua
+++ .config/nvim/lua/plugins/molten.lua
@@ -10,17 +10,17 @@ return {
             vim.g.molten_wrap_output = true
             vim.g.molten_auto_open_output = false
             vim.g.molten_enter_output_behavior = "open_and_enter"
-            vim.keymap.set("n", "<C-CR>", ":MoltenEvaluateLine<CR>", { silent=true, desc = "evaluate line"})
+            -- vim.keymap.set("n", "<C-CR>", ":MoltenEvaluateLine<CR>", { silent=true, desc = "evaluate line"})
             vim.keymap.set("v", "<C-CR>", ":<C-u>MoltenEvaluateVisual<CR>gv", { silent = true, desc = "evaluate visual selection" })
-            vim.keymap.set("n", ";", ":MoltenShowOutput<CR>", { silent=true, desc = "show output"})
-            vim.keymap.set("n", "q", ":MoltenHideOutput<CR>", { silent = true, desc = "hide output" })
-            vim.keymap.set("n", "<leader>mo", ":noautocmd MoltenEnterOutput<CR>", { silent = true, desc = "show/enter output" })
-            vim.keymap.set("n", "<C-o>", ":MoltenNext<CR>", { silent = true, desc = "go to next cell" })
-            vim.keymap.set("n", "<C-n>", ":MoltenPrev<CR>", { silent = true, desc = "go to previous cell" })
-            vim.keymap.set("n", "<leader>mi", ":MoltenInterrupt<CR>", { desc = "interrupt currently running cell" })
-            vim.keymap.set("n", "<leader>mr", ":MoltenRestart<CR>", { desc = "interrupt currently running cell" })
-            vim.keymap.set("n", "<leader>mim", ":MoltenImagePopup<CR>", { desc = "open image output in a default file viewer" })
-            vim.keymap.set('n', '<leader><CR>', ':MoltenReevaluateCell<CR>', { silent = true, desc = 're-evaluate cell' })
+            -- vim.keymap.set("n", ";", ":MoltenShowOutput<CR>", { silent=true, desc = "show output"})
+            -- vim.keymap.set("n", "q", ":MoltenHideOutput<CR>", { silent = true, desc = "hide output" })
+            -- vim.keymap.set("n", "<leader>mo", ":noautocmd MoltenEnterOutput<CR>", { silent = true, desc = "show/enter output" })
+            -- vim.keymap.set("n", "<C-o>", ":MoltenNext<CR>", { silent = true, desc = "go to next cell" })
+            -- vim.keymap.set("n", "<C-n>", ":MoltenPrev<CR>", { silent = true, desc = "go to previous cell" })
+            -- vim.keymap.set("n", "<leader>mi", ":MoltenInterrupt<CR>", { desc = "interrupt currently running cell" })
+            -- vim.keymap.set("n", "<leader>mr", ":MoltenRestart<CR>", { desc = "interrupt currently running cell" })
+            -- vim.keymap.set("n", "<leader>mim", ":MoltenImagePopup<CR>", { desc = "open image output in a default file viewer" })
+            -- vim.keymap.set('n', '<leader><CR>', ':MoltenReevaluateCell<CR>', { silent = true, desc = 're-evaluate cell' })
         end,
     },
     {
