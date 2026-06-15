976f5c43676942e220eff2684b4f619d7585367f
Artur Shevtsov <ashevtsov@luftronix.com>
2026-06-15 22:41:17 +0300

    [nvim]: rm molten.nvim

--- .config/nvim/ftplugin/molten_output.lua
+++
@@ -1,5 +0,0 @@
-local map = function(mode, lhs, rhs, opts)
-  vim.keymap.set(mode, lhs, rhs, vim.tbl_extend("force", { buffer = true, desc = "Molten" }, opts or {}))
-end
-map("n", ";", function() vim.cmd("MoltenShowOutput") end,          { desc = "Molten: Show output" })
-map("n", "q", function() vim.cmd("MoltenHideOutput") end,          { desc = "Molten: Hide output" })
