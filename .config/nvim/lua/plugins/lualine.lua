084f7f6b89b81c4a810d1b1f94542e86ebd5603e
Artur <ashevtsov@luftronix.com>
2026-06-16 14:55:16 +0300

    [nvim][lualine]: add unsaved changes indicator

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,9 +1,25 @@
 return {
   'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup({
       options = {
         theme = 'auto',
+        icons_enabled = true,
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {
+          {
+            'filename',
+            file_status = true,      -- This tracks if the file is written/modified
+            path = 1,                -- 0: Just filename, 1: Relative path, 2: Absolute path
+          }
+        },
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
       }
     })
   end
