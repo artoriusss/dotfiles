ad29eed660aed91e9fc2db7bd03941f2d3f3f298
Artur <ashevtsov@luftronix.com>
2026-06-17 18:30:34 +0300

    [nvim]: misc

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,17 @@ return {
   "folke/snacks.nvim",
   ---@type snacks.Config
   opts = {
-    notifier = {}
+    notifier = {
+      enabled = true,
+      timeout = 4000,
+    },
+    styles = {
+      notification = {
+        focusable = true,
+      }
+    }
+  },
+  keys = {
+    { "<leader>.n", function() Snacks.notifier.show_history() end, desc = "Notification History Buffer" },
   }
 }
