9516f7f02fd9390853cf4411fc9eb99526d7482b
Artur <ashevtsov@luftronix.com>
2026-06-18 18:39:51 +0300

    [nvim]: switch to snacks picker

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -6,6 +6,17 @@ return {
       enabled = true,
       timeout = 4000,
     },
+    picker = {
+      ui_select = true, -- replace vim.ui.select
+      win = {
+        input = {
+          keys = {
+            -- close immediately on <Esc> instead of going to normal mode
+            ["<Esc>"] = { "close", mode = { "n", "i" } },
+          },
+        },
+      },
+    },
     styles = {
       notification = {
         focusable = true,
@@ -14,5 +25,17 @@ return {
   },
   keys = {
     { "<leader>.n", function() Snacks.notifier.show_history() end, desc = "Notification History Buffer" },
+
+    -- Picker
+    { "<C-p>",      function() Snacks.picker.files() end,                   desc = "Pick: Find Files" },
+    { "<leader>fg", function() Snacks.picker.grep() end,                    desc = "Pick: Live Grep" },
+    { "<leader>b",  function() Snacks.picker.buffers() end,                 desc = "Pick: Buffers" },
+    { "<leader>s",  function() Snacks.picker.lsp_workspace_symbols() end,   desc = "Pick: Workspace Symbols" },
+    { "<leader>ts", function() Snacks.picker.lsp_symbols() end,            desc = "Pick: Document Symbols" },
+    {
+      "<leader>tf",
+      function() Snacks.picker.lsp_symbols({ filter = { default = { "Class", "Function", "Method" } } }) end,
+      desc = "Pick: Functions & Classes",
+    },
   }
 }
