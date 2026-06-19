e9618d60983657435f6941a7c8b05c1cb51a873c
Artur <ashevtsov@luftronix.com>
2026-06-19 11:53:54 +0300

    [nvim][lsp]: fix ruff & remove nodejs-dependent LSPs

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -12,11 +12,8 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
-          "ts_ls",
           "ruff",
           "ty",
-          "bashls",
-          "yamlls",
         },
       })
     end,
@@ -27,25 +24,17 @@ return {
     opts = {
       servers = {
         lua_ls = {},
-        ts_ls = {},
         ty = {},
-        ruff = {
-          init_options = { settings = {} },
-        },
-        bashls = {},
-        yamlls = {},
+        ruff = {},
       }
     },
     config = function(_, opts)
       for server, config in pairs(opts.servers) do
-        -- Inject blink.cmp capabilities into the config
         config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-        -- Native Neovim 0.11+ API configuration & initialization
         vim.lsp.config(server, config)
         vim.lsp.enable(server)
       end
 
-      -- LSP Keymaps
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Buffer Definition" })
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Buffer Code Action" })
       vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP: Rename symbol" })
