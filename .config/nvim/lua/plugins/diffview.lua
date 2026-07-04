b8cfd617d7729d2ea040c3a1ae901e821c4be681
Artur <ashevtsov@luftronix.com>
2026-07-04 19:51:48 +0300

    [nvim]: add diffview.nvim

---
+++ .config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,13 @@
+return {
+  "sindrets/diffview.nvim",
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview: open" },
+    { "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Diffview: close" },
+    { "<leader>gh", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview: repo history" },
+    { "<leader>gf", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview: current file history" },
+  },
+  config = function()
+    require("diffview").setup()
+  end,
+}
