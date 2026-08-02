335be19d631b8e00f510be94ad1a1fb339a85244
Artur <ashevtsov@luftronix.com>
2026-08-02 11:11:24 +0300

    [nvim][snacks]: toggle .gitignored files exclude from search

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,3 +1,18 @@
+-- Always silenced, even when .gitignored files are toggled back on.
+-- These go straight to fd/rg as excludes, independent of the ignore setting.
+local exclude = {
+  ".git",
+  "node_modules",
+  ".venv",
+  "venv",
+  "__pycache__",
+  ".mypy_cache",
+  ".pytest_cache",
+  ".ruff_cache",
+  "*.pyc",
+  ".DS_Store",
+}
+
 return {
   "folke/snacks.nvim",
   ---@type snacks.Config
@@ -10,6 +25,8 @@ return {
       ui_select = true, -- replace vim.ui.select
       sources = {
         buffers = { sort_lastused = true },
+        files = { exclude = exclude },
+        grep = { exclude = exclude },
       },
       win = {
         input = {
@@ -30,8 +47,16 @@ return {
     { "<leader>.n", function() Snacks.notifier.show_history() end, desc = "Notification History Buffer" },
 
     -- Picker
-    { "<C-p>",      function() Snacks.picker.files() end, desc = "Pick: Find Files" },
-    { "<leader>fg", function() Snacks.picker.grep() end, desc = "Pick: Live Grep" },
+    { "<C-p>",      function() Snacks.picker.files({ ignored = vim.g.snacks_ignored }) end, desc = "Pick: Find Files" },
+    { "<leader>fg", function() Snacks.picker.grep({ ignored = vim.g.snacks_ignored }) end, desc = "Pick: Live Grep" },
+    {
+      "<leader>ti",
+      function()
+        vim.g.snacks_ignored = not vim.g.snacks_ignored
+        Snacks.notify((vim.g.snacks_ignored and "Including" or "Excluding") .. " .gitignored files", { title = "Picker" })
+      end,
+      desc = "Pick: Toggle .gitignored files (session)",
+    },
     { "<leader>b",  function() Snacks.picker.buffers({ current = false }) end, desc = "Pick: Buffers" },
     { "<leader>s",  function() Snacks.picker.lsp_workspace_symbols() end, desc = "Pick: Workspace Symbols" },
     { "<leader>ts", function() Snacks.picker.lsp_symbols() end, desc = "Pick: Document Symbols" },
